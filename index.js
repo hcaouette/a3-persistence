2f0e550598dd1e43530559fe0ab26418924b0ec5
Hunter <huntercaouette@gmail.com>
2019-09-15 18:06:55 -0400

    server updates

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express   = require( 'express' ),
       app       = express(),
       cookiePaser = require( 'cookie-parser')
       cookieSession   = require( 'cookie-session' ),
+      session   = require( 'express-session' ),
       passport  = require( 'passport' ),
       Local     = require( 'passport-local' ).Strategy,
       bodyParser= require( 'body-parser' )
@@ -9,16 +10,9 @@ const express   = require( 'express' ),
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
-app.use( cookieSession({ secret:'cats cats cats', resave:false, saveUninitialized:false }) )
+app.use( session({ secret:'cats cats cats', resave:false, saveUninitialized:false }) )
 app.use( passport.initialize() )
 app.use( passport.session() )
-
-//redirects
-app.get(('/' || '/index.html'), (req, res) => res.sendFile(public/index.html))
-app.get('/cheeses.html', (req, res) => res.sendFile(public/cheeses.html))
-app.get('/about.html', (req, res) => res.sendFile(public/about.html))
-app.get('/cart.html', (req, res) => res.sendFile(public/cart.html))
-app.get('/login.html', (req, res) => res.sendFile(public/login.html))
 
 // a simple table to store non-persistent data. for assignment #3
 // your data must be persistent between sessions using a database (lowdb)
@@ -27,14 +21,33 @@ const users = [
   { username:'user', password:'userpass' }
 ]
 
-// all authentication requests in passwords assume that your client
-// is submitting a field named "username" and field named "password".
-// these are both passed as arugments to the authentication strategy.
+//redirects
+app.get(('/' || '/index.html'), (req, res) => res.sendFile(public/index.html))
+app.get('/cheeses.html', (req, res) => res.sendFile(public/cheeses.html))
+app.get('/about.html', (req, res) => res.sendFile(public/about.html))
+app.get('/cart.html', (req, res) => res.sendFile(public/cart.html))
+app.get('/login.html', (req, res) => res.sendFile(public/login.html))
+
+//catches request to '/login' from client
+app.post('/login', passport.authenticate( 'local' ), function( req, res ) {
+  console.log( 'user:', req.user )
+  res.json({ status:true })
+})
+//catches request to '/test' from client
+app.post('/test', function( req, res ) {
+  console.log( 'authenticate with cookie?', req.user )
+  res.json({ status:'success' })
+})
+
+
+
+/** all authentication requests in passwords assume that your client
+    is submitting a field named "username" and field named "password".
+    these are both passed as arugments to the authentication strategy.*/
 const myLocalStrategy = function( username, password, done ) {
   // find the first item in our users array where the username
   // matches what was sent by the client. nicer to read/write than a for loop!
   const user = users.find( __user => __user.username === username )
-
   // if user is undefined, then there was no match for the submitted username
   if( user === undefined ) {
     /* arguments to done():
@@ -44,6 +57,7 @@ const myLocalStrategy = function( username, password, done ) {
     */
     return done( null, false, { message:'user not found' })
   }else if( user.password === password ) {
+    console.log('user found')
     // we found the user and the password matches!
     // go ahead and send the userdata... this will appear as request.user
     // in all express middleware functions.
@@ -53,17 +67,10 @@ const myLocalStrategy = function( username, password, done ) {
     return done( null, false, { message: 'incorrect password' })
   }
 }
-
-
-
+//end myLocalStrategy func
 passport.use( new Local( myLocalStrategy ) )
 passport.initialize()
 
-app.post('/login', passport.authenticate( 'local' ), function( req, res ) {
-    console.log( 'user:', req.user )
-    res.json({ status:true })
-  }
-)
 
 passport.serializeUser( ( user, done ) => done( null, user.username ) )
 // "name" below refers to whatever piece of info is serialized in seralizeUser,
@@ -77,11 +84,6 @@ passport.deserializeUser( ( username, done ) => {
   }else{
     done( null, false, { message:'user not found; session not restored' })
   }
-})
-
-app.post('/test', function( req, res ) {
-  console.log( 'authenticate with cookie?', req.user )
-  res.json({ status:'success' })
 })
 
 
