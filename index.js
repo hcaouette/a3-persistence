961dfa86fa604d3ca57753efed8142017a5bb840
Hunter <huntercaouette@gmail.com>
2019-09-15 19:47:01 -0400

    oauth works

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use( passport.session() )
 passport.use(new GoogleStrategy({
     clientID: '443479129403-kgnq88arlnldecfi9qp87queha2fhl7r.apps.googleusercontent.com',
     clientSecret: 'RIPOr3cA2Vwyd00UiMNoxNfK',
-    callbackURL: "http://www.example.com/auth/google/callback"
+    callbackURL: "https://a3-hcaouette.glitch.me/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
        User.findOrCreate({ googleId: profile.id }, function (err, user) {
@@ -61,34 +61,6 @@ app.post('/test', function( req, res ) {
   res.json({ status:'success' })
 })
 
-
-
-/** all authentication requests in passwords assume that your client
-    is submitting a field named "username" and field named "password".
-    these are both passed as arugments to the authentication strategy.*/
-const myLocalStrategy = function( username, password, done ) {
-  // find the first item in our users array where the username
-  // matches what was sent by the client. nicer to read/write than a for loop!
-  const user = users.find( __user => __user.username === username )
-  // if user is undefined, then there was no match for the submitted username
-  if( user === undefined ) {
-    /* arguments to done():
-     - an error object (usually returned from database requests )
-     - authentication status
-     - a message / other data to send to client
-    */
-    return done( null, false, { message:'user not found' })
-  }else if( user.password === password ) {
-    console.log('user found')
-    // we found the user and the password matches!
-    // go ahead and send the userdata... this will appear as request.user
-    // in all express middleware functions.
-    return done( null, { username, password })
-  }else{
-    // we found the user but the password didn't match...
-    return done( null, false, { message: 'incorrect password' })
-  }
-}
 //end myLocalStrategy func
 passport.serializeUser( ( user, done ) => done( null, user.username ) )
 // "name" below refers to whatever piece of info is serialized in seralizeUser,
