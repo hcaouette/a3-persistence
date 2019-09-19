5c7b229275bc801ef7b9fb7961b1bc80990eb0ca
Hunter <huntercaouette@gmail.com>
2019-09-19 11:51:45 -0400

    script

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,28 +1,6 @@
 // Add some Javascript code here, to run on the front end.
 
 console.log("Welcome to assignment 3!")
-/**
-fetch( '/ ', {
-    method:'POST',
-    body:JSON.stringify({ username:'hc', password:'hc1' }),
-    credentials: 'include',
-    headers: { 'Content-Type': 'application/json' }
-  })
-  .then( res => res.json() )
-  .then( console.log )
-*/
-// window.onload = function () {
-//   fetch('/header', {method:'GET', credentials:'include'})
-//     .then((res) => {
-//     // .then((data) => {
-//       let header='',
-//       jBod = res.json()
-//       console.log(jBod)
-//       header+=(JSON.parse(jBod).cont)
-//       console.log(header)
-//     })
-// }
-
 
 const addToCart = function(chz){
   console.log('attempting addToCart')
